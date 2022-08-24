5fe1708f5c3a52ce6d719d79a875867e7c542891
mirelzashish <ashish@mirelz.com>
2022-08-24 15:38:19 +0530

    updated

--- firstapp/src/App.test.js
+++ firstapp/src/App.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
 
 it("renders", () => {
   const div = document.createElement("div");
